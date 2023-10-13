2c6911e631ceed00257d6060c153bf2d9e32a1db
mbageri <mohammadbageri@outlook.com>
2023-10-13 17:45:33 +0200

    split SecurityGroup module

--- main.tf
+++ main.tf
@@ -1,38 +1,5 @@
 data "aws_availability_zones" "all" {}
 
-resource "aws_security_group" "instance" {
-  name = "terraform-example-instance"
-  
-  ingress {
-    from_port	  = "${var.server_port}"
-    to_port		  = "${var.server_port}"
-    protocol	  = "tcp"
-    cidr_blocks	= ["0.0.0.0/0"]
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_security_group" "elb" {
-  name = "terraform-example-elb"
-  
-  ingress {
-    from_port	  = 80
-	  to_port		  = 80
-	  protocol	  = "tcp"
-	  cidr_blocks	= ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port	  = 0
-	  to_port		  = 0
-	  protocol	  = "-1"
-	  cidr_blocks	= ["0.0.0.0/0"]
-  }
-}
-
 resource "aws_launch_configuration" "example" {
   image_id		    = var.AMI_ID
   instance_type   = var.INSTANCE_TYPE
